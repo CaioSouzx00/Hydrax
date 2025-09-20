ffd9cf20740176b412a8dc01293dd557015227f0
YagoPaulino <yagopaulino04@gmail.com>
2025-09-20 16:49:23 -0300

    FEAT: Imagem na guia de todas as partes do usuario

--- resources/views/usuarios/carrinho.blade.php
+++ resources/views/usuarios/carrinho.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/imagens/hydrax/lch.png" type="image/png" />
     <title>Carrinho</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
