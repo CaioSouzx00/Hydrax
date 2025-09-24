2ef5727cf1f95081d4cf97694d48ed3980beea26
YagoPaulino <yagopaulino04@gmail.com>
2025-09-23 21:44:42 -0300

    FEAT: Imagens nas abas do admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" href="/imagens/hydrax/hydrax-perfil.png" type="image/png" />
+  <link rel="icon" href="/imagens/hydrax/lca.png" type="image/png" />
   <title>Dashboard Adm</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <!-- Particles.js CDN -->
